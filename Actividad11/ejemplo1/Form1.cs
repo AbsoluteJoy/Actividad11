0c2204e57f28b758369173a4ca314ad98c19b5c0
AbsoluteJoy <carlos_8185@hotmail.com>
2025-06-05 17:11:20 -0300

    ejemplo2

--- Actividad11/ejemplo1/Form1.cs
+++ Actividad11/ejemplo1/Form1.cs
@@ -2,31 +2,56 @@ namespace ejemplo1
 {
     public partial class Form1 : Form
     {
+        double acumulador = 0;
+        int contador = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
+
+
+
         private void button1_Click(object sender, EventArgs e)
         {
+
+
+
+
+            double valor = Convert.ToDouble(textBox1.Text);
+
+            acumulador += valor;
+
+            contador++;
+
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            double acumulador = 0
 
-            double valor = Convert.ToDouble(textBox1.Text)
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void btnPromedio_Click(object sender, EventArgs e)
         {
-
+            double promedio = acumulador / contador;//promedio
+            lbResultado.Text = $"{promedio,10:f2}";
+            textBox2.Text = $"{promedio,10:f2}";
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
--- Actividad11/ejemplo2/Form1.Designer.cs
+++ Actividad11/ejemplo2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ejemplo1
+﻿namespace ejemplo2
 {
     partial class Form1
     {
@@ -28,85 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnRegistrar = new Button();
-            textBox1 = new TextBox();
-            lbResultado = new Label();
-            textBox2 = new TextBox();
             btnPromedio = new Button();
+            textBox2 = new TextBox();
+            lbResultado = new Label();
+            textBox1 = new TextBox();
+            btnRegistrar = new Button();
             SuspendLayout();
             // 
-            // btnRegistrar
+            // btnPromedio
             // 
-            btnRegistrar.Location = new Point(462, 30);
-            btnRegistrar.Name = "btnRegistrar";
-            btnRegistrar.Size = new Size(118, 69);
-            btnRegistrar.TabIndex = 0;
-            btnRegistrar.Text = "Registrar";
-            btnRegistrar.UseVisualStyleBackColor = true;
-            btnRegistrar.Click += button1_Click;
+            btnPromedio.Location = new Point(437, 128);
+            btnPromedio.Name = "btnPromedio";
+            btnPromedio.Size = new Size(118, 60);
+            btnPromedio.TabIndex = 9;
+            btnPromedio.Text = "Calcular Promedio";
+            btnPromedio.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBox2
             // 
-            textBox1.Location = new Point(53, 30);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(344, 23);
-            textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
+            textBox2.Location = new Point(28, 206);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(344, 129);
+            textBox2.TabIndex = 8;
             // 
             // lbResultado
             // 
             lbResultado.AutoSize = true;
             lbResultado.BackColor = SystemColors.GradientInactiveCaption;
-            lbResultado.Location = new Point(81, 107);
+            lbResultado.Location = new Point(56, 102);
             lbResultado.Name = "lbResultado";
             lbResultado.Size = new Size(69, 15);
-            lbResultado.TabIndex = 2;
+            lbResultado.TabIndex = 7;
             lbResultado.Text = "lbResultado";
-            lbResultado.Click += label1_Click;
             // 
-            // textBox2
+            // textBox1
             // 
-            textBox2.Location = new Point(53, 211);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(344, 129);
-            textBox2.TabIndex = 3;
-            textBox2.TextChanged += textBox2_TextChanged;
+            textBox1.Location = new Point(28, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(344, 23);
+            textBox1.TabIndex = 6;
             // 
-            // btnPromedio
+            // btnRegistrar
             // 
-            btnPromedio.Location = new Point(462, 133);
-            btnPromedio.Name = "btnPromedio";
-            btnPromedio.Size = new Size(118, 60);
-            btnPromedio.TabIndex = 4;
-            btnPromedio.Text = "Calcular Promedio";
-            btnPromedio.UseVisualStyleBackColor = true;
-            btnPromedio.Click += btnPromedio_Click;
+            btnRegistrar.Location = new Point(437, 25);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(118, 69);
+            btnRegistrar.TabIndex = 5;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(623, 361);
+            ClientSize = new Size(583, 369);
             Controls.Add(btnPromedio);
             Controls.Add(textBox2);
             Controls.Add(lbResultado);
             Controls.Add(textBox1);
             Controls.Add(btnRegistrar);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
             Text = "Form1";
-            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button btnRegistrar;
-        private TextBox textBox1;
-        private Label lbResultado;
-        private TextBox textBox2;
         private Button btnPromedio;
+        private TextBox textBox2;
+        private Label lbResultado;
+        private TextBox textBox1;
+        private Button btnRegistrar;
     }
 }
